be0793367870b5b3e188a5d8989f5792cca8b2b3
yenhk1305 <92587395+yenhk1305@users.noreply.github.com>
2021-10-29 00:33:03 +0700

    Check roi

---
+++ app/src/main/java/com/example/adapter/AccountRecyclerAdapter.java
@@ -0,0 +1,24 @@
+package com.example.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AccountRecyclerAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>{
+    @NonNull
+    @Override
+    public RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
