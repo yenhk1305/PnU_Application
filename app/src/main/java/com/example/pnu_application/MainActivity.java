2795b9ca1c6e502180db299992618afd542dfe7a
thaonmp <thaonmp19411@st.uel.edu.vn>
2021-10-28 17:51:36 +0700

    hic

--- app/src/main/java/com/example/pnu_application/MainActivity.java
+++ app/src/main/java/com/example/pnu_application/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabCategory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
